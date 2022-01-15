23bc4d62b8b4b67521c150feb8411c2272e01670
Dalikk <dmitriev.alik1999@gmail.com>
2022-01-15 23:22:56 +0900

    Создана форма СВЯЗАННАЯ с моделью

--- testsite/mysite/news/forms.py
+++ testsite/mysite/news/forms.py
@@ -1,15 +1,17 @@
 from django import forms
-from .models import Category
+from .models import News
 
 
-class NewsForm(forms.Form):
-    title = forms.CharField(label="Название", max_length=150, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    content = forms.CharField(label="Текст", required=False, widget=forms.Textarea(
-        attrs={
-            'class': 'form-control',
-            'rows': 5,
-        }))
-    is_published = forms.BooleanField(label="Опубликовано", initial=True, required=False)
-    category = forms.ModelChoiceField(label="Категория", queryset=Category.objects.all(),
-                                      empty_label="Выберите категорию",
-                                      widget=forms.Select(attrs={'class': 'form-control'}))
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        #fields = '__all__'
+        fields = ('title', 'content', 'is_published', 'category')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 5,
+            }),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+        }
