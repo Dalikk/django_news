6e0de7e9c78eb2aea8dd0a802ac6c7b532e48fe8
Dalikk <dmitriev.alik1999@gmail.com>
2022-01-15 17:16:28 +0900

    Стилизованы поля формы

--- testsite/mysite/news/forms.py
+++ testsite/mysite/news/forms.py
@@ -3,7 +3,13 @@ from .models import Category
 
 
 class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150)
-    content = forms.CharField(widget=forms.Textarea)
-    is_published = forms.BooleanField(required=False)
-    category = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(label="Название", max_length=150, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    content = forms.CharField(label="Текст", required=False, widget=forms.Textarea(
+        attrs={
+            'class': 'form-control',
+            'rows': 5,
+        }))
+    is_published = forms.BooleanField(label="Опубликовано", initial=True, required=False)
+    category = forms.ModelChoiceField(label="Категория", queryset=Category.objects.all(),
+                                      empty_label="Выберите категорию",
+                                      widget=forms.Select(attrs={'class': 'form-control'}))
